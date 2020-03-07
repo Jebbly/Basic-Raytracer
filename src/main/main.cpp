939ae9455251886004a64d0b93843fa6334da186
Jeffrey Liu <jeffliu604@gmail.com>
2020-03-07 15:18:01 -0500

    Update Spheres Scene with Plane Primitive

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 #include "common.h"
 #include "math/tuple.h"
 #include "math/matrix.h"
@@ -32,26 +33,17 @@ int main()
 
     w.add_light(light);
 
-    Camera c{100, 50, Constants::PI / 3};
+    Camera c{1000, 500, Constants::PI / 3};
     Tuple from = point(0, 1.5, -5);
     Tuple to = point(0, 1, 0);
     Tuple up = vector(0, 1, 0);
     c.set_transform(view(from, to, up));
+    auto start = std::chrono::high_resolution_clock::now();
+    std::cout << "Rendering...\n";
     Framebuffer image = c.render(w);
+    auto stop = std::chrono::high_resolution_clock::now();
+    std::cout << "Finished in " << (std::chrono::duration_cast<std::chrono::seconds>(stop - start)).count() << " seconds\n";
     image.save_buffer("output.ppm");
-
-    Plane p{};
-    std::cout << normal(&p, point(0, 0, 0)) << "\n";
-    std::cout << normal(&p, point(10, 0, -10)) << "\n";
-    std::cout << normal(&p, point(-5, 0, 15)) << "\n";
-
-    std::vector<Intersection> xs = intersect(&p, Ray{point(0, 1, 0), vector(0, -1, 0)});
-    for (int i = 0; i < xs.size(); i++)
-	std::cout << xs.at(i).get_t() << '\n';
-
-    xs = intersect(&p, Ray{point(0, -1, 0), vector(0, 1, 0)});
-    for (int i = 0; i < xs.size(); i++)
-	std::cout << xs.at(i).get_t() << '\n';
 
     return 1;
 }
