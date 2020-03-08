7478b2f4a44afc4634220c426ba51781cdf25033
Jeffrey Liu <jeffliu604@gmail.com>
2020-03-08 13:19:02 -0400

    Restructured Examples

---
+++ examples/README.md
@@ -0,0 +1,6 @@
+# Example Renders
+The following renders are converted from PPM to PNG for the sake of compression.
+
+## Spheres
+- one_light (1000x500 - 1489 seconds)
+- two_lights (2000x1000 - 6934 seconds)
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -17,27 +17,27 @@ int main()
 {
     World w{};
 
-    Material floorMaterial{color(1, 0.9, 0.9), 0.1, 0.9, 0, 0};
+    Material floorMaterial{color(1, 0.9, 0.9), 0.1, 0.9, 1, 200};
     Plane floor{identity(), floorMaterial};
 
     w.add_object(floor);
 
-    Sphere middle{identity().translate(-0.5, 1, 0.5), Material{color(0.1, 1, 0.5), 0.1, 0.7, 0.3}};
-    Sphere right{identity().scale(0.5, 0.5, 0.5).translate(1.5, 0.5, -0.5), Material{color(0.5, 1, 0.1), 0.1, 0.7, 0.3}};
-    Sphere left{identity().scale(0.33, 0.33, 0.33).translate(-1.5, 0.33, -0.75), Material{color(1, 0.8, 0.1), 0.1, 0.7, 0.3}};
+    Sphere middle{identity().translate(-0.5, 1, 0.5), Material{color(0.1, 1, 0.5), 0.1, 0.7, 1, 200}};
+    Sphere right{identity().scale(0.5, 0.5, 0.5).translate(1.2, 0.5, -0.5), Material{color(0.5, 1, 0.1), 0.1, 0.7, 1, 32}};
+    Sphere left{identity().scale(0.33, 0.33, 0.33).translate(-1.5, 0.33, -0.75), Material{color(1, 0.8, 0.1), 0.1, 0.7, 1, 64}};
 
     w.add_object(middle);
     w.add_object(right);
     w.add_object(left);
 
-    PointLight light{color(1, 1, 1), point(-10, 10, -10)};
-    // PointLight light2{color(1, 1, 1), point(10, 10, -10)};
+    PointLight light{color(1, 1, 1), point(-2, 2, -2)};
+    PointLight light2{color(1, 1, 1), point(10, 10, -10)};
 
     w.add_light(light);
-    // w.add_light(light2);
+    w.add_light(light2);
 
-    Camera c{100, 50, Constants::PI / 3};
-    Tuple from = point(0, 1.5, -10);
+    Camera c{2000, 1000, Constants::PI / 3};
+    Tuple from = point(0, 1.5, -5);
     Tuple to = point(0, 1, 0);
     Tuple up = vector(0, 1, 0);
     c.set_transform(view(from, to, up));
