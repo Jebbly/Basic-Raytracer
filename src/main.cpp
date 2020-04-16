6615410f0d9878a0144538e594fc58ba7bb249ac
Jeffrey Liu <jeffliu604@gmail.com>
2020-04-15 20:27:33 -0400

    Add Demonstration Scene

--- src/main.cpp
+++ src/main.cpp
@@ -1,92 +1,79 @@
-#include <iostream>
 #include <chrono>
-#include "utility/constants.h"
+#include <conio.h>
+#include <iostream>
+#include <memory>
 #include "math/tuple.h"
 #include "math/matrix.h"
-#include "core/ray.h"
+#include "image/camera.h"
 #include "image/framebuffer.h"
-#include "geometry/primitives/sphere.h"
-#include "geometry/primitives/cube.h"
-#include "geometry/primitives/cylinder.h"
-#include "geometry/primitives/cone.h"
-#include "geometry/primitives/plane.h"
-#include "geometry/mesh.h"
 #include "geometry/group.h"
-#include "geometry/primitives/triangle.h"
-#include "geometry/csg.h"
-#include "geometry/primitives/smooth_triangle.h"
+#include "geometry/primitives/cylinder.h"
+#include "geometry/primitives/sphere.h"
+#include "geometry/primitives/plane.h"
 #include "material/color_material.h"
-#include "material/patterns/stripe_material.h"
+#include "material/patterns/checker_material.h"
 #include "material/patterns/gradient_material.h"
 #include "material/patterns/ring_material.h"
-#include "material/patterns/checker_material.h"
-#include "core/intersection.h"
+#include "material/patterns/stripe_material.h"
 #include "scene/lights/light.h"
 #include "scene/lights/point_light.h"
-#include "scene/lights/directional_light.h"
-#include "scene/lights/spot_light.h"
 #include "scene/world.h"
-#include "image/camera.h"
 
+// example render
 int main()
 {
-    scene::World w{};
+    // create world
+    scene::World world{};
 
+    // setup materials
     material::pattern::StripeMaterial stripes{image::Color{215 / 255.0, 178 / 255.0, 255 / 255.0}, image::Color{147 / 255.0, 216 / 255.0, 148 / 255.0}, math::identity().scale(0.5, 0.5, 0.5), 0.1, 0.9, 1, 200};
     material::pattern::GradientMaterial gradient{image::Color{0.8, 0, 0}, image::Color{0, 0, 0.8}, math::identity().scale(1.5, 1.5, 1.5).rotate_y(constants::PI / 3), 0.1, 0.9, 1, 200};
     material::pattern::RingMaterial ring{image::Color{211 / 255.0, 217 / 255.0, 231 / 255.0}, image::Color{141 / 255.0, 153 / 255.0, 186 / 255.0}, math::identity().scale(0.25, 0.25, 0.25).rotate_x(constants::PI / 1.5).rotate_y(-constants::PI / 12)};
     material::pattern::CheckerMaterial grid{image::Color{0.2, 0.2, 0.2}, image::Color{0.7, 0.7, 0.7}, math::identity(), 0.1, 0.9, 0.9, 200, 1};
-    material::ColorMaterial glass{image::Color{1, 1, 1}, 0, 0, 0.9, 200, 1.0, 1.0, 1.52};
-    material::ColorMaterial air{image::Color{1, 1, 1}, 0, 0, 0.9, 200, 1.0, 1.0, 1.0};
+
+    // create objects with materials
     geometry::primitive::Plane floor{math::identity(), std::make_shared<material::pattern::CheckerMaterial>(grid)};
+    geometry::primitive::Sphere middle{math::identity().translate(-0.5, 1, 0.5), std::make_shared<material::pattern::GradientMaterial>(gradient)};
+    geometry::primitive::Sphere right{math::identity().scale(0.5, 0.5, 0.5).translate(1.2, 0.5, -0.5), std::make_shared<material::pattern::RingMaterial>(ring)};
+    geometry::primitive::Sphere left{math::identity().scale(0.33, 0.33, 0.33).translate(-1.5, 0.33, -1.5).rotate_y(constants::PI / 4), std::make_shared<material::pattern::StripeMaterial>(stripes)};
+    
+    // group spheres together
+    geometry::Group spheres{};
+    spheres.add_child(&right);
+    spheres.add_child(&left);
+    spheres.add_child(&middle);
 
-    // w.add_object(floor);
+    // add objects to scene
+    world.add_object(floor);
+    world.add_object(spheres);
 
-    geometry::primitive::Sphere middle{math::identity().translate(-0.5, 1, 0.5), /*Material{image::Color{0.1, 1, 0.5), 0.1, 0.7, 1, 200}*/ std::make_shared<material::pattern::GradientMaterial>(gradient)};
-    geometry::primitive::Cone right{math::identity()./*scale(0.3, 0.5, 0.3).*/translate(1.2, 0, -0.5), /*Material{image::Color{0.5, 1, 0.1), 0.1, 0.7, 1, 32}*/ std::make_shared<material::pattern::RingMaterial>(ring), 0, 1, true};
-    geometry::primitive::Cylinder left{math::identity().scale(0.33, 0.33, 0.33).translate(-1.5, 0.33, -1.5).rotate_y(constants::PI / 4), /*Material{image::Color{1, 0.8, 0.1), 0.1, 0.7, 1, 64}*/ std::make_shared<material::pattern::StripeMaterial>(stripes), 0, 1, true};
-    geometry::Group g{};
+    // create lights
+    scene::light::PointLight key{image::Color{1, 1, 1}, math::point(-5, 5, -5)};
+    scene::light::PointLight fill{image::Color{1, 1, 1}, math::point(10, 10, -10)};
 
-    g.add_child(&right);
-    g.add_child(&left);
-    g.add_child(&middle);
+    // add lights to scene
+    world.add_light(key);
+    world.add_light(fill);
 
-    // Mesh mesh{"astronaut.obj", math::identity().scale(0.05, 0.05, 0.05).rotate_y(constants::PI / 4).translate(0.7, 2.7, 0.2)};
-
-    // Mesh test{"test.obj", math::identity()};
-    // w.add_object(test);
-
-    geometry::primitive::Sphere s{math::identity().scale(1.4, 1.4, 1.4).translate(0, 2, 0)};
-    geometry::primitive::Cube cy{math::identity().rotate_x(constants::PI / 2).rotate_y(constants::PI / 4).translate(0, 2, 0)};
-    geometry::CSG cs{&s, &cy, &geometry::csg_difference};
-    w.add_object(cs);
-
-    // w.add_object(middle);
-    // w.add_object(right);
-    // w.add_object(left);
-   // w.add_object(g);
-    // w.add_object(mesh);
-
-    // w.add_object(mainSphere);
-    // w.add_object(mainSphere2);
-
-    scene::light::PointLight light{image::Color{1, 1, 1}, math::point(-5, 5, -5)};
-    scene::light::PointLight light2{image::Color{1, 1, 1}, math::point(10, 10, -10)};
-
-    w.add_light(light);
-    w.add_light(light2);
-
-    image::Camera c{256, 144, constants::PI / 3};
-    math::Tuple4d from = math::point(0, 3, -9);
-    math::Tuple4d to = math::point(0, 2, 0);
+    // configure camera
+    image::Camera camera{1600, 800, constants::PI / 3};
+    math::Tuple4d from = math::point(0, 1.5, -5);
+    math::Tuple4d to = math::point(0, 1, 0);
     math::Tuple4d up = math::vector(0, 1, 0);
-    c.transform(math::view(from, to, up));
+    camera.transform(math::view(from, to, up));
+
+    // render scene to buffer
     auto start = std::chrono::high_resolution_clock::now();
-    std::cout << "Rendering...\n";
-    image::Framebuffer image = c.render(w);
+    std::cout << "Rendering image...\n";
+    image::Framebuffer image = camera.render(world);
     auto stop = std::chrono::high_resolution_clock::now();
-    std::cout << "Finished in " << (std::chrono::duration_cast<std::chrono::seconds>(stop - start)).count() << " seconds\n";
-    image.save_buffer("teapot.ppm");
+    std::cout << "Finished in " << (std::chrono::duration_cast<std::chrono::seconds>(stop - start)).count() << " seconds!\n";
+
+    // write buffer to file
+    image.save_buffer("example.ppm");
+    std::cout << "Press any key to exit.\n";
+    getch();
+
     return 0;
 }
 
